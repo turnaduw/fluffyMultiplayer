73881dd3e73e2784b7845ac90d4104d6badc30f0
hicplusplus <albin@senioradministrator.com>
2023-11-12 20:19:30 +0330

    enum AppState removed

--- main-client/include/dataType.h
+++ main-client/include/dataType.h
@@ -9,21 +9,6 @@ namespace FluffyMultiplayer
     boost::asio::ip::address ip;
     unsigned short port;
   };
-  enum AppStates
-  {
-    INIT_APP,
-    READING_SERVER_LIST,
-    INIT_SOCKET,
-    CONNECTING_TO_SERVER,
-
-    LOGIN_FORM,
-    SEND_LOGIN_DATA,
-    REGISTER_FROM,
-    SEND_REGISTER_DATA,
-    MAIN_PAGE,
-
-    LOGOUT
-  };
 }
 
 #endif
