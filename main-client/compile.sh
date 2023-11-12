2e9b3b62031a3353625cac0352537f2323bf2ac3
hicplusplus <albin@senioradministrator.com>
2023-11-12 20:19:03 +0330

    files inside states directory added into compile script

--- main-client/compile.sh
+++ main-client/compile.sh
@@ -1,5 +1,5 @@
 #compile whole program
-g++ main.cpp ./include/*.h ./src/*.cpp -o mc.run -std=c++11 -lboost_system -lboost_thread -lsfml-graphics -lsfml-window -lsfml-system -pthread -lpthread
+g++ main.cpp ./include/*.h ./include/states/*.h ./src/*.cpp ./src/states/*.cpp -o mc.run -std=c++11 -lboost_system -lboost_thread -lsfml-graphics -lsfml-window -lsfml-system -pthread -lpthread
 
 # g++ -c main.cpp ./include/*.h ./src/*.cpp
 rm *.o
