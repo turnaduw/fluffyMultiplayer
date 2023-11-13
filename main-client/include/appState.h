9ce4a37b204b99a7445482057b4fb34141680434
hicplusplus <albin@senioradministrator.com>
2023-11-13 13:19:33 +0330

    some unneseccery object and methods removed, later will replace some useful like createSprite(string path, texture, sprite) mixup those together to write clean and less code in render/constructor

--- main-client/include/appState.h
+++ main-client/include/appState.h
@@ -13,14 +13,6 @@ namespace FluffyMultiplayer
   class App;
   class AppState
   {
-  protected:
-    sf::Font basic_font;
-    unsigned int basic_text_size;
-    sf::Color basic_text_color;
-    sf::Texture bg_texture;
-    sf::Sprite bg_sprite;
-    std::string bg_path;
-    std::string font_path;
   public:
     virtual void render(sf::RenderWindow&)=0;
 
@@ -30,16 +22,6 @@ namespace FluffyMultiplayer
 
     virtual FluffyMultiplayer::AppState* eventHandle(FluffyMultiplayer::App&,
                                         sf::Event&)=0;
-
-    void setBasicFont(std::string);
-    void setBasicTextSize(unsigned int);
-    void setBasicTextColor(sf::Color);
-    void setBasicBackground(std::string);
-
-    sf::Sprite getBgSprite();
-    sf::Color getBasicColor();
-    sf::Font getBasicFont();
-    unsigned int getBasicTextSize();
   };
 }
 #endif
