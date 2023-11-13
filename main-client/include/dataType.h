f158db2915bb5188a8f34f77baa5400ceb53c07d
hicplusplus <albin@senioradministrator.com>
2023-11-13 13:16:46 +0330

    dataType lobbyData added.

--- main-client/include/dataType.h
+++ main-client/include/dataType.h
@@ -9,6 +9,19 @@ namespace FluffyMultiplayer
     boost::asio::ip::address ip;
     unsigned short port;
   };
+  struct LobbyData
+  {
+     bool isLocked;
+     bool isVoiceChatForbidden;
+     bool isTextChatForbidden;
+     bool isSpecterForbidden;
+     bool lobbyStatusInGame;
+     bool showLobbyInList;
+     int maxPlayers;
+     int currentPlayers;
+     int gameMode;
+     std::string address;
+  };
 }
 
 #endif
