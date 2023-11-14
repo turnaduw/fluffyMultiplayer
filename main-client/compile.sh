e6982bc5af4f65e59998098d67c49dcc991f2965
hicplusplus <albin@senioradministrator.com>
2023-11-14 15:13:01 +0330

    compile time were too long i tought to use flag -c first then use objecs but didnt work at all..

--- main-client/compile.sh
+++ main-client/compile.sh
@@ -1,6 +1,2 @@
 #compile whole program
 g++ main.cpp ./include/*.h ./include/states/*.h ./src/*.cpp ./src/states/*.cpp -o mc.run -std=c++11 -lboost_system -lboost_thread -lsfml-graphics -lsfml-window -lsfml-system -pthread -lpthread
-
-# g++ -c main.cpp ./include/*.h ./src/*.cpp
-rm *.o
-rm *.gch
