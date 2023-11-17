8416111eb6b9a6daa5f03b4a4a9319ba61add813
hicplusplus <albin@senioradministrator.com>
2023-11-17 20:41:59 +0330

    (state wait for response) updated to hold request data (needs this one while doing failed..), some other type of construcotrs added to use when calling it with more than one states.

--- main-client/include/states/stateWaitForResponse.h
+++ main-client/include/states/stateWaitForResponse.h
@@ -14,17 +14,25 @@ namespace FluffyMultiplayer
     int timeoutCounter;
     FluffyMultiplayer::AppState* state1;
     FluffyMultiplayer::AppState* state2;
+    FluffyMultiplayer::AppState* state3;
     int responseCodeAcceptor;
+    int responseCodeAcceptor2;
     std::string text;
-    std::string data;
-    bool buttonRetryPressed;
+    std::string receivedData;
+    std::string requestData;
     sf::Text timeouttxt;
+    bool requestSent;
     int checkResponseCode(const std::string&);
   public:
-    // StateWaitForResponse();
-    StateWaitForResponse(std::string text, FluffyMultiplayer::AppState* retryState,
+    StateWaitForResponse(std::string text,const std::string& request);
+    StateWaitForResponse(std::string text,const std::string& request, FluffyMultiplayer::AppState* retryState,
                          FluffyMultiplayer::AppState* acceptedState,
                          int responseCodeAccepts);
+    StateWaitForResponse(std::string text,const std::string& request, FluffyMultiplayer::AppState* retryState,
+                        FluffyMultiplayer::AppState* acceptedState,
+                        FluffyMultiplayer::AppState* acceptedState2,
+                        int responseCodeAccepts,int responseCodeAccepts2);
+
     ~StateWaitForResponse();
     void render(sf::RenderWindow&);
     FluffyMultiplayer::AppState* update(FluffyMultiplayer::App&,
--- main-client/src/states/stateWaitForResponse.cpp
+++ main-client/src/states/stateWaitForResponse.cpp
@@ -2,15 +2,16 @@
 
 namespace FluffyMultiplayer
 {
-  StateWaitForResponse::StateWaitForResponse(std::string _text, FluffyMultiplayer::AppState* retryState,
+  StateWaitForResponse::StateWaitForResponse(std::string _text,const std::string& request, FluffyMultiplayer::AppState* retryState,
                        FluffyMultiplayer::AppState* acceptedState,
                        int responseCodeAccepts)
   {
+    requestSent=false;
     timeoutCounter=MC_REQUEST_TIMEOUT;
     state1 = retryState;
     state2 = acceptedState;
     responseCodeAcceptor = responseCodeAccepts;
-    buttonRetryPressed=false;
+    requestData = request;
 
 
     std::string fontPath = MC_PATH_TO_FONTS MC_DEFAULT_FONT;
@@ -21,14 +22,25 @@ namespace FluffyMultiplayer
     timeouttxt.setString("timedout press enter to retry.");
   }
 
+  StateWaitForResponse::StateWaitForResponse(std::string text,const std::string& request, FluffyMultiplayer::AppState* retryState,
+                      FluffyMultiplayer::AppState* acceptedState,
+                      FluffyMultiplayer::AppState* acceptedState2,
+                      int responseCodeAccepts,int responseCodeAccepts2)
+  {
+    StateWaitForResponse(text,request,retryState,acceptedState,responseCodeAccepts);
+    responseCodeAcceptor2 = responseCodeAccepts2;
+    state3 = acceptedState2;
+  }
+
+
   StateWaitForResponse::~StateWaitForResponse()
   {
 
   }
 
-  int StateWaitForResponse::checkResponseCode(const std::string& data)
+  int StateWaitForResponse::checkResponseCode(const std::string& _data)
   {
-    std::string tempStr = data.substr(MC_RESPONSE_POSITION_MIN_INDEX,MC_RESPONSE_POSITION_MAX_INDEX);
+    std::string tempStr = _data.substr(MC_RESPONSE_POSITION_MIN_INDEX,MC_RESPONSE_POSITION_MAX_INDEX);
     const char* temp = tempStr.c_str();
     int code = std::atoi(temp);
     if(code>=MC_MINUMUM_RESPONSE_CODE)
@@ -54,6 +66,8 @@ namespace FluffyMultiplayer
                     std::queue<std::string>& sendDataQueue)
 
   {
+    if(requestSent==false)
+      sendDataQueue.push(requestData);
     //do counter minus to make timeout zero and left this state.
     if(timeoutCounter>0)
     {
@@ -63,10 +77,13 @@ namespace FluffyMultiplayer
       //check for received data.
       if(receiveDataQueue.size()>=1)
       {
-        data = receiveDataQueue.front();
+        receivedData = receiveDataQueue.front();
         receiveDataQueue.pop();
-        if(checkResponseCode(data) == responseCodeAcceptor)
+        if(checkResponseCode(receivedData) == responseCodeAcceptor)
           return state2; //accepted
+        else if(checkResponseCode(receivedData) == responseCodeAcceptor2
+                && state3!=nullptr)
+          return state3;
       }
     }
     return this; //keep this state
@@ -84,7 +101,12 @@ namespace FluffyMultiplayer
           if(event.key.code == sf::Keyboard::Enter || event.key.code == sf::Keyboard::Return)
           {
               if(timeoutCounter<=0)
+              {
+                // if(requestData.length()>=1)
+                  // return state1();
                 return state1;
+              }
+
           }
         }
         break;
