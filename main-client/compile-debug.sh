eb383b82711e778250f1e62e90e295824efacf94
hicplusplus <albin@senioradministrator.com>
2023-11-23 17:49:10 +0330

    write current date before and after compile to show how much toke time

--- main-client/compile-debug.sh
+++ main-client/compile-debug.sh
@@ -1,4 +1,4 @@
 #compile whole program for gdb
-g++ -c main.cpp ./include/*.h ./include/states/*.h ./src/*.cpp ./src/states/*.cpp
-
+date
 g++ -g main.cpp ./include/*.h ./include/states/*.h ./src/*.cpp ./src/states/*.cpp -o mc.run -std=c++11 -lboost_system -lboost_filesystem -lboost_thread -lsfml-graphics -lsfml-window -lsfml-system -pthread -lpthread
+date
--- main-client/compile.sh
+++ main-client/compile.sh
@@ -1,2 +1,4 @@
 #compile whole program
+date
 g++ main.cpp ./include/*.h ./include/states/*.h ./src/*.cpp ./src/states/*.cpp -o mc.run -std=c++11 -lboost_system -lboost_filesystem -lboost_thread -lsfml-graphics -lsfml-window -lsfml-system -pthread -lpthread
+date
