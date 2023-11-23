0314c35b138957850014ced7421d3b8bcc23bbee
hicplusplus <albin@senioradministrator.com>
2023-11-23 17:59:53 +0330

    stateFailed was show nothing (i mean that window.draw(theText) which were simple and main text to display were not displayed so that problem fixed, and some event keypressed added to make ways into others states have to.

--- main-client/src/states/stateFailed.cpp
+++ main-client/src/states/stateFailed.cpp
@@ -2,7 +2,7 @@
 
 namespace FluffyMultiplayer
 {
-  StateFailed::StateFailed(std::string text,FluffyMultiplayer::AppState* s,std::string* request=nullptr)
+  StateFailed::StateFailed(std::string _text,FluffyMultiplayer::AppState* s,std::string* request=nullptr)
   {
     outputStateCount=1;
     requestData = request;
@@ -11,25 +11,32 @@ namespace FluffyMultiplayer
     state3=nullptr;
     requestData=request;
     std::string fontPath = MC_PATH_TO_FONTS MC_DEFAULT_FONT;
-    initSimpleText(fontPath, "Failed: "+text);
+    text = "Failed: "+_text;
+    initSimpleText(fontPath, text);
   }
 
-  StateFailed::StateFailed(std::string text,FluffyMultiplayer::AppState* s1,
+  StateFailed::StateFailed(std::string _text,FluffyMultiplayer::AppState* s1,
                            FluffyMultiplayer::AppState* s2,std::string* request=nullptr)
  {
-   StateFailed(text,s1,request);
+   StateFailed(_text,s1,request);
    outputStateCount=2;
    state2=s2;
    state3=nullptr;
+   std::string fontPath = MC_PATH_TO_FONTS MC_DEFAULT_FONT;
+   text = "Failed: "+_text;
+   initSimpleText(fontPath, text);
  }
 
-  StateFailed::StateFailed(std::string text,FluffyMultiplayer::AppState* s1,
+  StateFailed::StateFailed(std::string _text,FluffyMultiplayer::AppState* s1,
                            FluffyMultiplayer::AppState* s2,
                            FluffyMultiplayer::AppState* s3,std::string* request=nullptr)
   {
-    StateFailed(text,s1,s2,request);
+    StateFailed(_text,s1,s2,request);
     outputStateCount=3;
     state3=s3;
+    std::string fontPath = MC_PATH_TO_FONTS MC_DEFAULT_FONT;
+    text = "Failed: "+_text;
+    initSimpleText(fontPath, text);
   }
 
 
@@ -49,23 +56,6 @@ namespace FluffyMultiplayer
                     std::queue<std::string>& sendDataQueue)
 
   {
-    switch (outputStateCount)
-    {
-      case 1:
-      {
-
-      }break;
-
-      case 2:
-      {
-
-      }break;
-
-      case 3:
-      {
-
-      }break;
-    }
     return this;
   }
 
@@ -73,6 +63,43 @@ namespace FluffyMultiplayer
   FluffyMultiplayer::AppState* StateFailed::eventHandle(FluffyMultiplayer::App& app,
                             sf::Event& event)
   {
+    switch(event.type)
+    {
+      //keyboard
+        case sf::Event::KeyPressed:
+        {
+          switch (outputStateCount)
+          {
+            case 1:
+            {
+              if(state1!=nullptr)
+                return state1;
+            }break;
+            case 2:
+            {
+              if(state1!=nullptr && state2!=nullptr)
+              {
+                if(event.key.code == sf::Keyboard::Enter)
+                  return state1;
+                else
+                  return state2;
+              }
+            }break;
+            case 3:
+            {
+              if(state1!=nullptr && state2!=nullptr && state3!=nullptr)
+              {
+                if(event.key.code == sf::Keyboard::Enter)
+                  return state1;
+                else if(event.key.code == sf::Keyboard::Space)
+                  return state2;
+                else
+                  return state3;
+              }
+            }break;
+          }//switch end
+        }break;
+    }//switch end
     return this;
   }
 }
