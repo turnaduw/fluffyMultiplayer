a812533007ec4247e0436c06ad8fd29b15c5b77a
hicplusplus <albin@senioradministrator.com>
2023-11-27 05:28:54 +0330

    compile script updated, dir uiComponents added.

--- main-client/compile.sh
+++ main-client/compile.sh
@@ -1,4 +1,4 @@
 #compile whole program
 date
-g++ main.cpp ./include/*.h ./include/states/*.h ./src/*.cpp ./src/states/*.cpp -o mc.run -std=c++11 -lboost_system -lboost_filesystem -lboost_thread -lsfml-graphics -lsfml-window -lsfml-system -pthread -lpthread
+g++ main.cpp ./include/*.h ./include/states/*.h ./include/uiComponents/*.h ./src/*.cpp ./src/states/*.cpp -o mc.run -std=c++11 -lboost_system -lboost_filesystem -lboost_thread -lsfml-graphics -lsfml-window -lsfml-system -pthread -lpthread
 date
