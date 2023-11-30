7f5d431bf0044dccf61b4b77b15917744a8463a4
hicplusplus <albin@senioradministrator.com>
2023-11-30 21:30:15 +0330

    in past commit i changed wrong line, so fixed now.

--- main-client/src/states/stateLoginForm.cpp
+++ main-client/src/states/stateLoginForm.cpp
@@ -8,9 +8,9 @@ namespace FluffyMultiplayer
     std::string fontPath = MC_PATH_TO_FONTS MC_DEFAULT_FONT;
     initSimpleText(fontPath, "LOGIN FORM");
     setSimpleTextPosition(150.0, 5.0);
-    saveLoginCheckBox.init("save login", 300.0, 300.0, sf::Color::Black, sf::Color::White, 60,30, 22);
+    saveLoginCheckBox.init("save login", 300.0, 300.0, sf::Color::Black, sf::Color::White, 22);
     buttonGoToRegisterForm.init("Sign up", 100.0,400.0, sf::Color::Black,sf::Color::White, 60,30, 22);
-    buttonSubmit.init("submit", 300.0,400.0, sf::Color::Black,sf::Color::Green, 22);
+    buttonSubmit.init("submit", 300.0,400.0, sf::Color::Black,sf::Color::Green, 60,30, 22);
   }
 
   FluffyMultiplayer::AppState* StateLoginForm::formFinishedResult(bool isSubmit)
