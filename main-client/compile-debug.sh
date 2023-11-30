e3e7a93982d673a8d67d9ab220778913ba8bdb71
hicplusplus <albin@senioradministrator.com>
2023-11-30 14:19:53 +0330

    compile commands replaced with a bash script to do both normal and debug type of compile with some prints to be more friendly looks like.

--- main-client/compile-debug.sh
+++
@@ -1,4 +0,0 @@
-#compile whole program for gdb
-date
-g++ -g main.cpp ./include/*.h ./include/states/*.h ../include/uiComponents/*.h /src/*.cpp ./src/states/*.cpp -o mc.run -std=c++11 -lboost_system -lboost_filesystem -lboost_thread -lsfml-graphics -lsfml-window -lsfml-system -pthread -lpthread
-date
--- main-client/compile.sh
+++ main-client/compile.sh
@@ -1,4 +1,26 @@
-#compile whole program
-date
-g++ main.cpp ./include/*.h ./include/states/*.h ./include/uiComponents/*.h ./src/*.cpp ./src/states/*.cpp -o mc.run -std=c++11 -lboost_system -lboost_filesystem -lboost_thread -lsfml-graphics -lsfml-window -lsfml-system -pthread -lpthread
-date
+#!/bin/bash
+compileMain="main.cpp ./include/*.h ./include/states/*.h ./include/uiComponents/*.h ./src/*.cpp ./src/states/*.cpp -o mc.run -std=c++11 -lboost_system -lboost_filesystem -lboost_thread -lsfml-graphics -lsfml-window -lsfml-system -pthread -lpthread"
+PS3="Select compile type: "
+select compileMode in normal debug
+do
+  case $REPLY in
+    1)
+          #compile whole program
+          echo -e "\n\e[31m[START]\e[39m compile $compileMode started at: `date`"
+          g++ $compileMain
+          echo -e "\n\e[32m[END]\e[39m compile $compileMode finished at: `date`"
+          break
+          ;;
+    2)
+          #compile whole program for gdb
+          echo -e "\n\e[31m[START]\e[39m compile $compileMode started at: `date`"
+          g++ -g $compileMain
+          echo -e "\n\e[32m[END]\e[39m compile $compileMode finished at: `date`"
+          break
+          ;;
+    *)
+          echo "incorrect index."
+          # break
+          ;;
+  esac
+done
