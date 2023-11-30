d5e7d16572845c2557d40ef5b73b31853e2c7e93
hicplusplus <albin@senioradministrator.com>
2023-11-30 15:15:13 +0330

    script compile updated, to do run application with input number 3.

--- main-client/compile.sh
+++ main-client/compile.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 compileMain="main.cpp ./include/*.h ./include/states/*.h ./include/uiComponents/*.h ./src/*.cpp ./src/states/*.cpp -o mc.run -std=c++11 -lboost_system -lboost_filesystem -lboost_thread -lsfml-graphics -lsfml-window -lsfml-system -pthread -lpthread"
 PS3="Select compile type: "
-select compileMode in normal debug
+select compileMode in normal debug run
 do
   case $REPLY in
     1)
@@ -9,13 +9,15 @@ do
           echo -e "\n\e[31m[START]\e[39m compile $compileMode started at: `date`"
           g++ $compileMain
           echo -e "\n\e[32m[END]\e[39m compile $compileMode finished at: `date`"
-          break
           ;;
     2)
           #compile whole program for gdb
           echo -e "\n\e[31m[START]\e[39m compile $compileMode started at: `date`"
           g++ -g $compileMain
           echo -e "\n\e[32m[END]\e[39m compile $compileMode finished at: `date`"
+          ;;
+    3)
+          ./mc.run
           break
           ;;
     *)
