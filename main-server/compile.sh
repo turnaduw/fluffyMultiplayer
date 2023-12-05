575dfff73e7d0d0416022cbbf8c325efca7e10d2
hicplusplus <albin@senioradministrator.com>
2023-12-05 22:50:47 +0330

    script compile updated, now supports debug and normal mode with two option run normal and run via gdb.

--- main-server/compile.sh
+++ main-server/compile.sh
@@ -1,5 +1,38 @@
-#compile whole program
-rm fluffy.db
-g++ main.cpp ./include/*.h ./src/*.cpp -o ms.run -std=c++11 -lboost_regex -lboost_system -lboost_filesystem -lboost_thread -lsqlite3
-rm *.o
-rm *.gch
+#!/bin/bash
+compileMain="main.cpp ./include/*.h ./src/*.cpp -o ms.run -std=c++11 -lboost_regex -lboost_system -lboost_filesystem -lboost_thread -lsqlite3"
+PS3="Select compile type: "
+select compileMode in normal debug run runViaGDB
+do
+  case $REPLY in
+    1)
+          #compile whole program
+          echo -e "\n\e[31m[START]\e[39m compile $compileMode started at: `date`"
+          rm fluffy.db
+          g++ $compileMain
+          rm *.o
+          rm *.gch
+          echo -e "\n\e[32m[END]\e[39m compile $compileMode finished at: `date`"
+          ;;
+    2)
+          #compile whole program for gdb
+          echo -e "\n\e[31m[START]\e[39m compile $compileMode started at: `date`"
+          rm fluffy.db
+          g++ -g $compileMain
+          rm *.o
+          rm *.gch
+          echo -e "\n\e[32m[END]\e[39m compile $compileMode finished at: `date`"
+          ;;
+    3)
+          ./ms.run
+          break
+          ;;
+    4)
+          gdb ./ms.run
+          break
+          ;;
+    *)
+          echo "incorrect index."
+          # break
+          ;;
+  esac
+done
