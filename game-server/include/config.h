c4d132c057228b031b21d3d6b7c10977bcebf56e
hicplusplus <albin@senioradministrator.com>
2023-12-22 20:11:10 +0330

    class log added to write acitities on log file also be easy to turn off/on or fileter message types on print on consoles.

--- game-server/include/config.h
+++ game-server/include/config.h
@@ -5,7 +5,10 @@
 #define MC_RECEIVE_BUFFER 1024
 #define MS_DATA_ENCRYPT_DECRYPT_SHIFT 5
 
-#define DEFAULT_PORT_TEXT 8889
-#define DEFAULT_PORT_VOICE 8890
+#define DEFAULT_PORT_TEXT 7777
+#define DEFAULT_PORT_VOICE 9999
+
+#define PRINT_LOGS_LEVEL FluffyMultiplayer::LogType::all
+#define LOG_FILE "./gs.log"
 
 #endif
---
+++ game-server/src/log.cpp
@@ -0,0 +1,82 @@
+#include "../include/log.h"
+
+namespace FluffyMultiplayer
+{
+  void Log::print(std::string text, FluffyMultiplayer::LogType messageType)
+  {
+    std::string mtype;
+    switch (messageType)
+    {
+      case FluffyMultiplayer::LogType::Information: mtype = "[INFO] "; break;
+      case FluffyMultiplayer::LogType::Error: mtype = "[ERROR] "; break;
+      case FluffyMultiplayer::LogType::Warning: mtype = "[WARNING] "; break;
+      case FluffyMultiplayer::LogType::Success: mtype = "[SUCCESS] ";break;
+      default: mtype = "[?]";break;
+    }
+
+    try
+    {
+      text = mtype + getTime() + " " + text + "\n";
+      file << text;
+
+      //print on console
+      if(levelPrintOnConsole == FluffyMultiplayer::LogType::All
+          || messageType == levelPrintOnConsole)
+      {
+        std::cout << text;
+      }
+    }
+    catch (std::exception& e)
+    {
+      if(printOnConsole)
+        std::cout << "failed to write on log file.\ttext="<< text;
+    }
+  }
+
+  std::string Log::getTime()
+  {
+      // Get current time
+      time_t now = time(0);
+      tm* ltm = localtime(&now);
+
+      std::string time = "[" + std::to_string(1900 + ltm->tm_year) + "/" +
+              std::to_string(1 + ltm->tm_mon) + "/" +
+              std::to_string(ltm->tm_mday) + " " +
+              std::to_string(ltm->tm_hour) + ":" +
+              std::to_string(ltm->tm_min) + ":" +
+              std::to_string(ltm->tm_sec) + "]";
+
+      return time;
+  }
+
+  Log::Log()
+  {
+    levelPrintOnConsole = PRINT_LOGS_LEVEL;
+    filename = LOG_FILE;
+    try
+    {
+      file.open (filename, std::ofstream::out | std::ofstream::app);
+    }
+    catch (std::exception& e)
+    {
+      std::cout << "could not open/create log file." << std::endl;
+    }
+  }
+
+  Log::~Log()
+  {
+    try
+    {
+      if(file.is_open())
+        file.close();
+      else
+        std::cout << "file log is not open can not close it." << std::endl;
+    }
+    catch (std::exception& e)
+    {
+      std::cout << "could not close log file." << std::endl;
+    }
+  }
+
+
+}
