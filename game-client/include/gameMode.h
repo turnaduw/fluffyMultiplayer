3f0e77a5f444de566c85751907fd2989f36c9606
hicplusplus <albin@senioradministrator.com>
2024-02-15 06:27:34 +0330

    a flag added to let gameMode turn it on when game Mode wants render his conents by app

--- game-client/include/gameMode.h
+++ game-client/include/gameMode.h
@@ -30,7 +30,15 @@ namespace FluffyMultiplayer
     protected:
       int gameModeId;
 
+      //a flag to turn on when game started,  App will render game in some case gameMode before start has bad position of elemnts
+      //so this flag make game mode to avoid that render before,
+      bool gameStarted;
+
     public:
+      bool getGameStarted() const
+      {
+        return gameStarted;
+      }
       virtual void updatePlayersInGame(std::vector<std::string>& receivedPlayersInGameInfo,int myId)=0;
       virtual FluffyMultiplayer::GameMode* update(int& currentItemCode, std::vector<std::string>& cData)=0;
       virtual void render(sf::RenderWindow& window)=0;
